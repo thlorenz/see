3752b830bc8610501cc54874c09ca71cd69c754d
Thorsten Lorenz <thlorenz@gmx.de>
2012-10-11 21:34:06 -0400

    fixed match tests and started moving spike into sectionize module

--- test/match.js
+++ test/match.js
@@ -4,23 +4,26 @@
 
 var test = require('tap').test
   , tap  = require('tap-stream')
+  , fs = require('fs')
   , path = require('path')
-  , readContentAndFindMatchingLines = require('../lib/matcher')
+  , match = require('../lib/match')
   , root = path.join(__dirname, 'fixtures') 
   , foofunction = path.join(root, 'foo-function.js')
   ;
 
 test('matching foo-function.js for bar', function (t) {
-  readContentAndFindMatchingLines('bar') 
+  match('bar') 
     .on('data', function (data) {
-      t.deepEquals(data.matches[0].lineno, 2, 'lineno of first match')
+      t.deepEquals(data.matches[0].lineno, 3, 'lineno of first match')
       t.deepEquals(data.matches[0].column, 6, 'column of first match')
 
-      t.deepEquals(data.matches[1].lineno, 4, 'lineno of second match')
+      t.deepEquals(data.matches[1].lineno, 5, 'lineno of second match')
       t.deepEquals(data.matches[1].column, 9, 'column of second match')
 
       t.equals(data.lines.length, 5, 'lines contain all lines in file')
       t.equals(data.lines[0], 'function foo () {', 'text of first line')
+
+      t.equals(data.content, fs.readFileSync(foofunction, 'utf-8').trim())
 
       t.end();
     }) 
