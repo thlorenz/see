570e9c7f54ec6c9b52f29518cfb64c696e4d67f8
Thorsten Lorenz <tlorenz@lab49.com>
2012-10-12 18:13:46 -0400

    manually tested flow

--- grejs.js
+++ grejs.js
@@ -1,16 +1,27 @@
-var readdirp =  require('readdirp')
-  , tap      =  require('tap-stream')
-  , log      =  require('npmlog')
-  , es       =  require('event-stream')
-  , findMatchingLines  =  require('./lib/matcher')
+var readdirp   =  require('readdirp')
+  , tap        =  require('tap-stream')
+  , log        =  require('npmlog')
+  , es         =  require('event-stream')
+  , match      =  require('./lib/match')
+  , sectionize =  require('./lib/sectionize')
   ;
 
 function grejs (opts) {
   readdirp(opts)
     .on('error', function (err) { log.error('readdirp', err); })
     .on('warn', function (err) { log.warn('readdirp', err); })
-    .pipe(findMatchingLines(opts.term))
-    .pipe(es.mapSync(function (info) { return { path: info.file.path, matches: info.matches }; }))
+    .pipe(match(opts.term))
+    .pipe(es.mapSync(
+      function (info) { 
+        try {
+          var sections = sectionize(info.content, info.matches);
+          return { path: info.file.path, sections: sections };
+        } catch (e) {
+          //log.error('sectionize', e);
+          return undefined;
+        }
+      })
+    )
     .pipe(tap(2))
     ;
 }
